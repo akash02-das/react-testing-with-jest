501666a919223f168cce0197280959cd8215134e
akash das <aku.das02@gmail.com>
2023-08-30 00:16:41 +0600

    provider with mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
 import './App.css';
 import Application from './components/application/Application';
 import Counter from './components/counter/Counter';
+import { MuiMode } from './components/mui/mui-mode';
 import Skills from './components/skills/Skills';
+import { AppProviders } from './providers/app-providers';
 
 function App() {
   const skills = ['React', 'JavaScript', 'TypeScript'];
 
   return (
-    <div className='App'>
-      <Application />
-      <Skills skills={skills} />
-      <Counter />
-    </div>
+    <AppProviders>
+      <div className='App'>
+        <Application />
+        <Skills skills={skills} />
+        <Counter />
+        <MuiMode />
+      </div>
+    </AppProviders>
   );
 }
 
